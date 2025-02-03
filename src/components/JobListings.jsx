bb6823159cc66498b4f0bc71db38bed2594242b8
William Toussaint <william@outsidethebox.dev>
2025-02-03 11:16:50 -0700

    font text color change

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -6,7 +6,7 @@ const JobListings = () => {
   return (
     <section className="bg-gray-500 px-4 py-10">
       <div className="container-xl lg:container m-auto">
-        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+        <h2 className="text-3xl font-bold text-black-500 mb-6 text-center">
           Ways to Connect! 
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
